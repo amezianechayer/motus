89101e10b59dbf2d2c825152e4c9ec5afce51523
amezianechayer <amezianechayer9@gmail.com>
2024-06-12 02:09:36 +0200

    Ajouter le header

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import './header.css';
+
+export default function Header() {
+    return (
+      <div className='header-container'>
+      <div className='header-text'>Motus Laplateforme</div>
+      </div>
+    )
+}
