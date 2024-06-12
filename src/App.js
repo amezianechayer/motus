f99ce56f21a2b0ef4d920be276d78dbe950ebc0c
amezianechayer <amezianechayer9@gmail.com>
2024-06-12 03:28:02 +0200

    Faire la grille

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './app.css';
+import Grid from './components/grid';
 import Header from './components/header';
 function App() {
+  const width = 6
+  const height = 6
   return (
     <div className="app-container">
       <Header/>
+      <Grid width={width} height={height} />
     </div>
   );
 }
